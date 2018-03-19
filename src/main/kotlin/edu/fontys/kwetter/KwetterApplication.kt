5fcb7845a4a3377deda9dce8e09c1826d82dce22
Ivo Huntjens <gamerkeeperson+github@gmail.com>
2018-03-19 16:39:51 +0100

    Initialize project via Spring Initializr

---
+++ src/main/kotlin/edu/fontys/kwetter/KwetterApplication.kt
@@ -0,0 +1,11 @@
+package edu.fontys.kwetter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KwetterApplication
+
+fun main(args: Array<String>) {
+    runApplication<KwetterApplication>(*args)
+}
