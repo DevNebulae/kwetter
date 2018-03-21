fc379d49c4d1d1a98e3946fd90186786fc9b84b7
Ivo Huntjens <gamerkeeperson+github@gmail.com>
2018-03-21 19:56:20 +0100

    Add post mapping for account registration

--- src/main/kotlin/edu/fontys/kwetter/thymeleaf/controllers/LoginController.kt
+++ src/main/kotlin/edu/fontys/kwetter/thymeleaf/controllers/LoginController.kt
@@ -1,14 +1,20 @@
 package edu.fontys.kwetter.thymeleaf.controllers
 
 import edu.fontys.kwetter.account.AccountDto
+import edu.fontys.kwetter.account.AccountService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.context.request.WebRequest
 
 @Controller
 class LoginController {
+    @Autowired
+    private lateinit var accountService: AccountService
+
     @GetMapping("/login")
     fun index(): String {
         return "login"
@@ -16,8 +22,15 @@ class LoginController {
 
     @GetMapping("/register")
     fun register(model: Model): String {
-        val dto = AccountDto()
+        val dto = AccountDto("", "", "")
         model.addAttribute("account", dto)
         return "registration"
+    }
+
+    @PostMapping("/register")
+    fun registerAccount(@ModelAttribute("account") dto: AccountDto): String {
+        accountService.register(dto)
+
+        return index()
     }
 }
