dce0b0c176c798454db9d79a58613f93082140d2
Ivo Huntjens <gamerkeeperson+github@gmail.com>
2018-03-21 19:53:47 +0100

    Remove annotations from Account entity DTO

--- src/main/kotlin/edu/fontys/kwetter/account/AccountDto.kt
+++ src/main/kotlin/edu/fontys/kwetter/account/AccountDto.kt
@@ -1,14 +1,7 @@
 package edu.fontys.kwetter.account
 
-import javax.validation.constraints.NotBlank
-
 data class AccountDto(
-        @field: NotBlank
         var handle: String,
-
-        @field: NotBlank
         var username: String,
-
-        @field: NotBlank
         var password: String
 )
