e8cae80fb473df6e12134d8d5f920a49162b9fc8
DevNebulae <gamerkeeperson+github@gmail.com>
2018-03-21 17:04:49 +0100

    Update WebSecurityConfig with new roles and to use a login service

--- src/main/kotlin/edu/fontys/kwetter/config/WebSecurityConfig.kt
+++ src/main/kotlin/edu/fontys/kwetter/config/WebSecurityConfig.kt
@@ -1,20 +1,34 @@
 package edu.fontys.kwetter.config
 
+import edu.fontys.kwetter.account.authentication.AccountDetailService
+import edu.fontys.kwetter.account.role.AccountRole
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+
 
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfig : WebSecurityConfigurerAdapter() {
-    protected override fun configure(http: HttpSecurity) {
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    override fun configure(http: HttpSecurity) {
         http
                 .authorizeRequests()
-                .antMatchers("/**/*.html", "/**/*.css", "favicon.ico")
+                .antMatchers("/**/*.html", "/**/*.css", "/favicon.ico", "/register")
                 .permitAll()
                 .anyRequest()
-                .authenticated()
+                .hasRole(AccountRole.ADMIN.toString())
                 .and()
                 .formLogin()
                 .loginPage("/login")
@@ -22,5 +36,10 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
                 .and()
                 .logout()
                 .permitAll()
+    }
+
+    @Autowired
+    fun configureGlobal(auth: AuthenticationManagerBuilder, userDetailService: AccountDetailService) {
+        auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder())
     }
 }
