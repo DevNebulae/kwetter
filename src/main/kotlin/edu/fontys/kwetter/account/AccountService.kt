61a21f15a94aa1f0c08171559be0c8b785178fe2
DevNebulae <gamerkeeperson+github@gmail.com>
2018-03-21 17:03:17 +0100

    Add register method to Account service

--- src/main/kotlin/edu/fontys/kwetter/account/AccountService.kt
+++ src/main/kotlin/edu/fontys/kwetter/account/AccountService.kt
@@ -1,8 +1,8 @@
 package edu.fontys.kwetter.account
 
-import edu.fontys.kwetter.account.Account
-import edu.fontys.kwetter.account.AccountRepository
+import edu.fontys.kwetter.account.role.AccountRole
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,10 +10,20 @@ class AccountService {
     @Autowired
     private lateinit var repository: AccountRepository
 
+    @Autowired
+    private lateinit var passwordEncoder: PasswordEncoder
+
     fun follow(follower: Account, toBeFollowed: Account) {
         follower.following.add(toBeFollowed)
         toBeFollowed.followers.add(follower)
 
         repository.saveAll(listOf(follower, toBeFollowed))
+    }
+
+    fun register(dto: AccountDto): Account {
+        val account = Account(dto.handle, dto.username, passwordEncoder.encode(dto.password))
+        account.roles.add(AccountRole.USER)
+
+        return repository.save(account)
     }
 }
