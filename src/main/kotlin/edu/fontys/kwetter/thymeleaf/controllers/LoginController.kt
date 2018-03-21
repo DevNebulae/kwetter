d78684eb32927ce96a32464386e38dbe19833ee3
DevNebulae <gamerkeeperson+github@gmail.com>
2018-03-21 13:34:00 +0100

    Add route for registration form to LoginController

--- src/main/kotlin/edu/fontys/kwetter/thymeleaf/controllers/LoginController.kt
+++ src/main/kotlin/edu/fontys/kwetter/thymeleaf/controllers/LoginController.kt
@@ -1,13 +1,23 @@
 package edu.fontys.kwetter.thymeleaf.controllers
 
+import edu.fontys.kwetter.account.AccountDto
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.context.request.WebRequest
 
 @Controller
 class LoginController {
     @GetMapping("/login")
     fun index(): String {
         return "login"
+    }
+
+    @GetMapping("/register")
+    fun register(model: Model): String {
+        val dto = AccountDto()
+        model.addAttribute("account", dto)
+        return "registration"
     }
 }
