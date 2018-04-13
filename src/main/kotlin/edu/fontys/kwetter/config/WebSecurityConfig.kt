d84a5ce941f30c3590a2787d017382a06c5be00e
DevNebulae <gamerkeeperson+github@gmail.com>
2018-04-13 23:22:54 +0200

    Add OAuth2 Java configuration

--- src/main/kotlin/edu/fontys/kwetter/config/WebSecurityConfig.kt
+++ src/main/kotlin/edu/fontys/kwetter/config/WebSecurityConfig.kt
@@ -3,6 +3,7 @@ package edu.fontys.kwetter.config
 import edu.fontys.kwetter.account.authentication.AccountDetailService
 import edu.fontys.kwetter.account.role.AccountRole
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
@@ -11,31 +12,38 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI
+
 
 @Configuration
 @EnableWebSecurity
-class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+class WebSecurityConfig {
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return BCryptPasswordEncoder()
     }
 
-    override fun configure(http: HttpSecurity) {
-        http
-                .authorizeRequests()
-                .antMatchers("!register/**")
-                .hasRole(AccountRole.USER.toString())
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll()
-    }
+    @Bean
+    fun webSecurityConfigurer(@Value("\${keycloak-client.realm}") realm: String): WebSecurityConfigurerAdapter {
+        return object : WebSecurityConfigurerAdapter() {
+            public override fun configure(http: HttpSecurity) {
+                http
+                        .csrf()
+                        .disable()
 
-    @Autowired
-    fun configureGlobal(auth: AuthenticationManagerBuilder, userDetailService: AccountDetailService) {
-        auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder())
+                http
+                        .authorizeRequests()
+                        .anyRequest()
+                        .authenticated()
+                        .and()
+                        // This is the point where OAuth2 login of Spring 5 gets enabled
+                        .oauth2Login()
+                        // I don't want a page with different clients as login options
+                        // So i use the constant from OAuth2AuthorizationRequestRedirectFilter
+                        // plus the configured realm as immediate redirect to Keycloak
+                        .loginPage("$DEFAULT_AUTHORIZATION_REQUEST_BASE_URI/$realm")
+            }
+        }
     }
 }
