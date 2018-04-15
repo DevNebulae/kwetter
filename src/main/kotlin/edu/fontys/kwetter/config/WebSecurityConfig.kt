74edbc2a9f8eac4ccb4d15308b081aee8f778a0b
Ivo Huntjens <gamerkeeperson@gmail.com>
2018-04-15 14:23:22 +0200

    Clean up configuration and add disable CSRF and CORS

--- src/main/kotlin/edu/fontys/kwetter/config/WebSecurityConfig.kt
+++ src/main/kotlin/edu/fontys/kwetter/config/WebSecurityConfig.kt
@@ -1,26 +1,50 @@
 package edu.fontys.kwetter.config
 
+import org.keycloak.adapters.KeycloakConfigResolver
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter
+import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.core.session.SessionRegistryImpl
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
-import org.keycloak.adapters.KeycloakConfigResolver
-
-
 
 
 @KeycloakConfiguration
 class WebSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
-    @Throws(Exception::class)
+    @Bean
+    fun keycloakAuthenticationProcessingFilterRegistrationBean(filter: KeycloakAuthenticationProcessingFilter): FilterRegistrationBean {
+        val registrationBean = FilterRegistrationBean(filter);
+        registrationBean.setEnabled(false);
+        return registrationBean;
+    }
+
+    @Bean
+    fun keycloakPreAuthActionsFilterRegistrationBean(filter: KeycloakPreAuthActionsFilter): FilterRegistrationBean {
+        val registrationBean = FilterRegistrationBean(filter);
+        registrationBean.setEnabled(false);
+        return registrationBean;
+    }
+
     override fun configure(http: HttpSecurity) {
         super.configure(http)
+
         http
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionAuthenticationStrategy(sessionAuthenticationStrategy())
+                .and()
                 .authorizeRequests()
                 .anyRequest()
                 .authenticated()
@@ -30,13 +54,14 @@ class WebSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
      * Registers the KeycloakAuthenticationProvider with the authentication manager.
      */
     @Autowired
-    @Throws(Exception::class)
     fun configureGlobal(auth: AuthenticationManagerBuilder) {
-        auth.authenticationProvider(keycloakAuthenticationProvider())
+        val provider = keycloakAuthenticationProvider()
+        provider.setGrantedAuthoritiesMapper(SimpleAuthorityMapper())
+        auth.authenticationProvider(provider)
     }
 
     @Bean
-    fun KeycloakConfigResolver(): KeycloakConfigResolver {
+    fun keycloakConfigResolver(): KeycloakConfigResolver {
         return KeycloakSpringBootConfigResolver()
     }
 
@@ -45,6 +70,6 @@ class WebSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
      */
     @Bean
     override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy {
-        return RegisterSessionAuthenticationStrategy(SessionRegistryImpl())
+        return NullAuthenticatedSessionStrategy()
     }
 }
