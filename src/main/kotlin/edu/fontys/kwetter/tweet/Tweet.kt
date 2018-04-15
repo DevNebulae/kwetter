1d1f8871fdfe1262b3a0211bae6972cd5b74e489
Ivo Huntjens <gamerkeeperson@gmail.com>
2018-04-15 14:26:50 +0200

    Refactor Tweet to work with Keycloak

--- src/main/kotlin/edu/fontys/kwetter/tweet/Tweet.kt
+++ src/main/kotlin/edu/fontys/kwetter/tweet/Tweet.kt
@@ -4,20 +4,23 @@ import edu.fontys.kwetter.account.Account
 import org.hibernate.validator.constraints.NotBlank
 import java.io.Serializable
 import javax.persistence.*
+import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 @Entity
 class Tweet(
-        @field: ManyToOne(optional = false)
-        val author: Account,
-
         @field: NotBlank
-        @field: Size(max = 140)
-        val content: String
+        @field: Size(max = CHARACTER_LIMIT)
+        val content: String,
+
+        author: String?
 ) : Serializable {
     @Id
     @GeneratedValue
     val id: Long = 0
+
+    @NotNull
+    val author = author
 
     @ManyToMany(mappedBy = "liked")
     val likes: MutableSet<Account> = mutableSetOf()
