654e63566167d789dc2cb66de742fd5d11e1834f
Ivo Huntjens <gamerkeeperson@gmail.com>
2018-04-15 14:27:35 +0200

    Disable TweetRepository as a REST resource

--- src/main/kotlin/edu/fontys/kwetter/tweet/TweetRepository.kt
+++ src/main/kotlin/edu/fontys/kwetter/tweet/TweetRepository.kt
@@ -1,8 +1,6 @@
 package edu.fontys.kwetter.tweet
 
-import edu.fontys.kwetter.tweet.Tweet
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
-@RepositoryRestResource(path = "/tweets")
 interface TweetRepository : JpaRepository<Tweet, Long>
