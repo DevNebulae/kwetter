8633ea57b00deb4be22d06636848160a51018d66
Ivo Huntjens <gamerkeeperson@gmail.com>
2018-04-22 22:03:13 +0200

    Add findByAuthor method to TweetRepository

--- src/main/kotlin/edu/fontys/kwetter/tweet/TweetRepository.kt
+++ src/main/kotlin/edu/fontys/kwetter/tweet/TweetRepository.kt
@@ -3,4 +3,6 @@ package edu.fontys.kwetter.tweet
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
-interface TweetRepository : JpaRepository<Tweet, Long>
+interface TweetRepository : JpaRepository<Tweet, Long> {
+    fun findByAuthor(author: String): List<Tweet>
+}
