6290643cefc34f4ab12201a5193fa3ba8662073f
Ivo Huntjens <gamerkeeperson@gmail.com>
2018-04-25 07:25:18 +0200

    Add Keycloak bean to WebSecurityConfig

--- src/main/kotlin/edu/fontys/kwetter/config/WebSecurityConfig.kt
+++ src/main/kotlin/edu/fontys/kwetter/config/WebSecurityConfig.kt
@@ -1,28 +1,51 @@
 package edu.fontys.kwetter.config
 
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder
 import org.keycloak.KeycloakSecurityContext
 import org.keycloak.adapters.KeycloakConfigResolver
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration
+import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticatedActionsFilter
 import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter
 import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter
+import org.keycloak.adapters.springsecurity.filter.KeycloakSecurityContextRequestFilter
+import org.keycloak.admin.client.Keycloak
+import org.keycloak.admin.client.KeycloakBuilder
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.config.ConfigurableBeanFactory
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Scope
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
 import org.springframework.security.core.context.SecurityContext
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.session.SessionRegistryImpl
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 
 
 @KeycloakConfiguration
-class WebSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
+class WebSecurityConfig : KeycloakWebSecurityConfigurerAdapter {
+    @Autowired
+    private lateinit var keycloakClientRequestFactory: KeycloakClientRequestFactory
+
+    @Value("\${keycloak.auth-server-url}")
+    private lateinit var keycloakServerURL: String
+
+    constructor() : super() {
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL)
+    }
+
     override fun configure(http: HttpSecurity) {
         super.configure(http)
 
@@ -38,6 +61,37 @@ class WebSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
                 .authorizeRequests()
                 .anyRequest()
                 .authenticated()
+    }
+
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     */
+    @Autowired
+    fun configureGlobal(auth: AuthenticationManagerBuilder) {
+        val provider = keycloakAuthenticationProvider()
+        provider.setGrantedAuthoritiesMapper(SimpleAuthorityMapper())
+        auth.authenticationProvider(provider)
+    }
+
+    @Bean
+    fun keycloak(): Keycloak {
+        return KeycloakBuilder
+                .builder()
+                .serverUrl(keycloakServerURL)
+                .realm("master")
+                .username("admin")
+                .password("admin")
+                .clientId("admin-cli")
+                .resteasyClient(ResteasyClientBuilder().connectionPoolSize(10).build())
+                .build()
+    }
+
+    @Bean
+    fun keycloakAuthenticatedActionsFilterBean(filter: KeycloakAuthenticatedActionsFilter): FilterRegistrationBean {
+        val registrationBean = FilterRegistrationBean(filter);
+        registrationBean.setEnabled(false);
+
+        return registrationBean;
     }
 
     @Bean
@@ -61,14 +115,18 @@ class WebSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
         return registrationBean;
     }
 
-    /**
-     * Registers the KeycloakAuthenticationProvider with the authentication manager.
-     */
-    @Autowired
-    fun configureGlobal(auth: AuthenticationManagerBuilder) {
-        val provider = keycloakAuthenticationProvider()
-        provider.setGrantedAuthoritiesMapper(SimpleAuthorityMapper())
-        auth.authenticationProvider(provider)
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    fun keycloakRestTemplate(): KeycloakRestTemplate {
+        return KeycloakRestTemplate(keycloakClientRequestFactory)
+    }
+
+    @Bean
+    fun keycloakSecurityContextRequestFilterBean(filter: KeycloakSecurityContextRequestFilter): FilterRegistrationBean {
+        val registrationBean = FilterRegistrationBean(filter);
+        registrationBean.setEnabled(false);
+
+        return registrationBean;
     }
 
     /**
@@ -76,6 +134,6 @@ class WebSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
      */
     @Bean
     override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy {
-        return NullAuthenticatedSessionStrategy()
+        return RegisterSessionAuthenticationStrategy(SessionRegistryImpl());
     }
 }
